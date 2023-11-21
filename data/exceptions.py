aefa0dd1618d9d8d67a1eb68bcab37e833013099
MattWay224 <112853687+MattWay224@users.noreply.github.com>
2023-11-21 04:02:08 +0300

    update exceptions

--- data/exceptions.py
+++ data/exceptions.py
@@ -10,5 +10,8 @@ class Exceptions:
         assert isinstance(s, Vector), "Supply is not a vector"
         assert a.getHeight() == s.getWidth(), "Length of supply vector does not correspond to # of rows of matrix A"
         assert a.getWidth() == d.getWidth(), "Length of demand vector does not correspond to # of cols of matrix A"
-        assert all(x >= 0 for x in s.getVector()), "Supply vector should be non-negative"
-        assert all(x >= 0 for x in d.getVector()), "Demand vector should be non-negative"
+        assert all(x >= 0 for x in s.getVector()), "Supply vector values should be positive"
+        assert all(x >= 0 for x in d.getVector()), "Demand vector values should be positive"
+        for i in range(a.getHeight()):
+            assert all(x >= 0 for x in a.getMatrix()[i]), "Matrix values should be positive"
+        assert d.sumV() >= s.sumV(), "Infeasible solution: supply vector cannot be bigger than demand vector"
